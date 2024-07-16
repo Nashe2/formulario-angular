484df3dd647e18e5f098878ad8b5b6b9a41481b2
Nashe Osorio <nasheog2@gmail.com>
2024-07-16 11:58:50 -0600

    Validaciones a un FormControl

--- src/app/components/basic-form/basic-form.component.ts
+++ src/app/components/basic-form/basic-form.component.ts
@@ -1,32 +1,39 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialComponentModule } from '../../material-component/material-component.module';
-import { FormControl, ReactiveFormsModule } from '@angular/forms';
-import { LayoutComponent } from "../../layout/layout.component";
+import {
+  ReactiveFormsModule,
+  Validators,
+  FormControl
+} from '@angular/forms';
+import { LayoutComponent } from '../../layout/layout.component';
 import { RouterModule } from '@angular/router';
-/* import {ThemePalette} from '@angular/material/core'; */
-
 @Component({
   selector: 'app-basic-form',
   standalone: true,
-  imports: [CommonModule, MaterialComponentModule, ReactiveFormsModule, RouterModule, LayoutComponent],
+  imports: [
+    CommonModule,
+    MaterialComponentModule,
+    ReactiveFormsModule,
+    RouterModule,
+    LayoutComponent,
+  ],
   templateUrl: './basic-form.component.html',
-  styleUrl: './basic-form.component.scss'
+  styleUrl: './basic-form.component.scss',
 })
 export class BasicFormComponent {
 
-  nameControl = new FormControl('');
-  emailControl = new FormControl('');
-  phoneControl = new FormControl('');
-  colorControl = new FormControl('#000000');
-  dateControl = new FormControl('');
+  nameControl= new FormControl('', Validators.required);
+  emailControl= new FormControl('', Validators.email);
+  phoneControl= new FormControl('', [Validators.required, Validators.maxLength(10)]);
 
-  escolarControl = new FormControl('bachillerato');
-  pasatiempoControl = new FormControl('');
-  estadoControl = new FormControl('soltero');
+  colorControl= new FormControl('#000000');
+  dateControl= new FormControl('');
+  escolarControl= new FormControl('bachillerato');
+  pasatiempoControl= new FormControl('');
+  estadoControl= new FormControl('soltero', Validators.required);
 
-  genderControl = new FormControl('');
-  policyControl = new FormControl(false);
-  terminoControl = new FormControl(false);
-  /* tagControl = new FormControl('primary' as ThemePalette); */
+  genderControl= new FormControl('');
+  policyControl= new FormControl(false, Validators.required);
+  terminoControl= new FormControl(false, Validators.required);
 }
